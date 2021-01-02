29b613260182a29421fa9bd3ac702eb8d93f3e1f
leejuye <0811j@naver.com>
2021-01-03 00:36:03 +0900

    Add react-native-paper

--- client/App.tsx
+++ client/App.tsx
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider, Badge } from 'react-native-paper';
 
 export default function App() {
   return (
+    <PaperProvider>
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>test1</Text>
+      <StatusBar style="auto" /> */}
+      <Badge visible>3</Badge>
     </View>
+    </PaperProvider>
   );
 }
 
