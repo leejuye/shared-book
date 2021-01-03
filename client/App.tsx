0c41a7dff5abe8b054be4aecaf769f77ce68c7cb
leejuye <0811j@naver.com>
2021-01-03 12:13:57 +0900

    Change navigation types

--- client/App.tsx
+++ client/App.tsx
@@ -1,21 +1,8 @@
 import React from 'react';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
-
 import Navigation from './component/shared/Navigation'
 
 export default function App() {
   return (
-    <PaperProvider theme={theme}>
       <Navigation />
-    </PaperProvider>
   );
 }
-
-const theme = {
-  ...DefaultTheme,
-  roundness: 2,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: '#4f3c75',
-  },
-};
--- client/component/shared/Navigation.tsx
+++ client/component/shared/Navigation.tsx
@@ -1,29 +1,43 @@
-import React, { useState } from 'react';
-import { BottomNavigation } from 'react-native-paper';
+import React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons'
 
 import Home from '../home/Home'
-import Friend from '../friend/Friend'
 import My from '../my/My'
+import Friend from '../friend/Friend'
 
-export default function Navigation() {
-  const [index, setIndex] = useState(0);
-  const [routes] = useState([
-    { key: 'home', icon: 'home' },
-    { key: 'friend', icon: 'charity' },
-    { key: 'my', icon: 'account' },
-  ]);
-
-  const renderScene = BottomNavigation.SceneMap({
-    home: Home,
-    friend: Friend,
-    my: My,
-  });
-
+export default function App() {
   return (
-    <BottomNavigation
-      navigationState={{ index, routes }}
-      onIndexChange={setIndex}
-      renderScene={renderScene}
-    />
+    <NavigationContainer>
+      <Tab.Navigator
+				screenOptions={({ route }) => ({    
+					// {focused,color,position}
+					tabBarLabel: ({focused})=>focused&&<Text style={{color: '#fff', marginLeft: 20, fontWeight: '600'}}>{route.name}</Text>,
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons name={iconName[route.name as keyof typeof iconName]} size={size} color={color} />;
+          },
+        })} 
+				tabBarOptions={{
+					activeTintColor: '#fff', 
+					inactiveTintColor: 'gray',
+					activeBackgroundColor: '#4f3c75',
+					tabStyle: {borderRadius:50, margin: 5, padding: 5 },
+					labelPosition: "beside-icon"
+				}}>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Friend" component={Friend} />     
+        <Tab.Screen name="My" component={My} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-} ;
+}
+
+const Tab = createBottomTabNavigator();
+
+const iconName = {
+	Home: 'home-outline',
+	My: 'person-outline',
+	Friend: 'heart-outline'
+}
