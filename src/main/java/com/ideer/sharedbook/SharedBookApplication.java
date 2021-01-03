fefb80b3438f009bf4c0a0df832754d70bffcd8e
orihehe <38060133+orihehe@users.noreply.github.com>
2021-01-03 18:19:22 +0900

    Initial commit for server setting

---
+++ src/main/java/com/ideer/sharedbook/SharedBookApplication.java
@@ -0,0 +1,13 @@
+package com.ideer.sharedbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharedBookApplication.class, args);
+    }
+
+}
