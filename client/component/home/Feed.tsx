49b5c8d85ac6166d33cbd5448794c173a7dcc21d
leejuye <0811j@naver.com>
2021-01-04 22:29:17 +0900

    Add Home feed

---
+++ client/component/home/Feed.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
+import AutoHeightImage from 'react-native-auto-height-image';
+
+import TmpAvatar from '../../assets/tmpAvatar.png'
+
+const windowWidth = Dimensions.get('window').width;
+
+interface Props {
+	id: number,
+	userName?: string,
+	userPhoto?: string,
+	book?: { title: string, author?: string, id: string },
+	photo?: string
+	text?: string
+}
+
+export default function Feed(props: Props) {
+	const {id, userName, userPhoto, book, photo, text} = props
+
+  return (
+    <View style={styles.container}>
+			<View style={styles.userInfo}>
+				<Image style={styles.userPhoto} source={userPhoto?userPhoto:TmpAvatar}/>
+				<Text style={styles.userName}>{userName}</Text>
+			</View>
+			<View>
+				{photo &&
+					<AutoHeightImage
+						style={styles.photo}
+						width={windowWidth - 30}
+						source={{uri: photo }}
+					/>
+				}
+				<Text style={styles.content}>{text}</Text>
+			</View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+		marginBottom: 10,
+		padding: 15,
+		width: '100%',
+	},
+
+	userInfo: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		marginBottom: 5
+	},
+	userPhoto: {
+		width: 30,
+		height: 30,
+		borderRadius: 50,
+	},
+	userName: {
+		marginLeft: 10,
+		fontWeight: '600',
+		fontSize: 20,
+	},
+
+	photo: {
+		marginTop: 10,
+		marginBottom: 10
+	},
+	content: {
+		marginTop: 10
+	}
+});
--- client/component/home/Home.tsx
+++ client/component/home/Home.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity } from "react-native";
+
+import Feed from './Feed'
+
+const DATA = [
+  {id: 1, userName: "leejuye", text: "떡국 먹고 싶다.", photo: 'https://i.ytimg.com/vi/EfRGTCyk6nU/maxresdefault.jpg', liked: true},
+  {id: 2, userName: "joy", text: "생각 난 영단어가 tree여서 tree를 쳐서 사진을 따왔다. 뭔가 긴 글로 테스트해봐야하는데 긴 글이 생각이 안나네. 진짜 긴 글이여야한느데 텔미 텔미 테테테테테테텔미 누난 너무 예뻐 미쳐 베이벱 베이벱 베이벱", photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Linde_von_linn.jpg/285px-Linde_von_linn.jpg'},
+  {id: 3, userName: "orihehe", text: "text test 3", liked: true},
+  {id: 4, userName: "orihehe", text: "text test 3"},
+  {id: 5, userName: "orihehe", text: "text test 3"},
+  {id: 6, userName: "orihehe", text: "text test 3"},
+]
+
 
 export default function Home() {
+  const renderItem = ({item}:any) => <Feed {...item}/>
+
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+      />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: '#fff'
   },
 });
